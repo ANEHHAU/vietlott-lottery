b03a5959843c276478669e9a0354410f5406b2a6
ANEHHAU <maitrunghaugit29817998@gmail.com>
2026-04-18 18:59:18 +0700

    lk

--- src/main/java/com/vietlott/lottery/vietlott/repository/LotteryPredictionRepository.java
+++
@@ -1,6 +0,0 @@
-package com.vietlott.lottery.vietlott.repository;
-
-import com.vietlott.lottery.vietlott.entity.LotteryPrediction;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LotteryPredictionRepository extends JpaRepository<LotteryPrediction, Long> {}
